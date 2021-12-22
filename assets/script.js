4f73d5b85aeb6d5eee16e3fb4416199295b75e8a
wyork <w.york63@gmail.com>
2021-12-22 14:22:42 -0500

    notes added to js and index

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+// make variables for all of the time block objects that will hold data
+
+
+// text entered into input boxes in html will be saved when the button is clicked
+
+
+
+// background color changes depending on what time it is 
+    // . past .present .future classes in css get 
+    // function will add the class to the the block that has time approaching 
+
+
+
