c3ad13d71e17da4d0ba4a0b7553a3d3200761516
wyork <w.york63@gmail.com>
2021-12-22 14:39:31 -0500

    rearranged files

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,9 @@
 
 // text entered into input boxes in html will be saved when the button is clicked
 
+//display todays date on page 
 
+$("#currentDay").text(moment().format("dddd, MMMM Do"));
 
 // background color changes depending on what time it is 
     // . past .present .future classes in css get 
