300b700c64df919f1e0d2f80de1b525b56864cc2
wyork <w.york63@gmail.com>
2021-12-22 17:05:34 -0500

    change colors and save info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,41 @@
-// make variables for all of the time block objects that will hold data
-var timeblocks = {
-    9: "",
-    10: "",
-    11: "",
-    12: "",
-    1: "",
-    2: "",
-    3: "",
-    4: "",
-    5: "",
-};
+var currentHour = moment().hours()
 
-// text entered into input boxes in html will be saved when the button is clicked
 
 //display todays date on page 
-
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
 // background color changes depending on what time it is 
     // . past .present .future classes in css 
     // function will add the class to the the block that has time approaching 
+function updateCss() {
+    $(".time-block").each(function(){
+       var divTime = $(this).attr("id");
+       if (divTime < currentHour) {
+           $(this).removeClass("present futre")
+           $(this).addClass("past")
+       }
+       else if (divTime > currentHour) {
+           $(this).removeClass("past present")
+           $(this).addClass("future")
+       }
+       else {
+           $(this).removeClass("future past")
+           $(this).addClass("present")
+       }
+    }) 
+}
 
-
+updateCss();
 // store text enetered into input when clicking the save button
+$(".saveBtn").click(function(event) {
+    event.preventDefault(); 
+    var timeKey = $(this).parent().attr("id");
+    var content = $(this).siblings(".description").val();
+
+    localStorage.setItem(timeKey, content)
+})
 
 // use var timeblocks to display what info had been entered into text area 
 
+$("#9 .description").val(localStorage.getItem("9"))
+// copy above function and alternate different numbers in 
