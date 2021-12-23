ea88b64489e6d711cff5f511af80d9ba8fe7e7cf
wyork <w.york63@gmail.com>
2021-12-23 11:43:58 -0500

    added screenshot and updated readme

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,4 +38,12 @@ $(".saveBtn").click(function(event) {
 // use var timeblocks to display what info had been entered into text area 
 
 $("#9 .description").val(localStorage.getItem("9"))
-// copy above function and alternate different numbers in 
+$("#10 .description").val(localStorage.getItem("10"))
+$("#11 .description").val(localStorage.getItem("11"))
+$("#12 .description").val(localStorage.getItem("12"))
+$("#13 .description").val(localStorage.getItem("13"))
+$("#14 .description").val(localStorage.getItem("14"))
+$("#15 .description").val(localStorage.getItem("15"))
+$("#16 .description").val(localStorage.getItem("16"))
+$("#17 .description").val(localStorage.getItem("17"))
+
